8991db914f835f9a62231830feb934879cb23f5e
Yoga <baolixiong82329@gmail.com>
2024-07-25 15:12:55 +0800

    add code

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const num = ref(2)
+function clickMe() {
+  num.value++
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="abcc">
+    <div>hello ^^ v</div>
+    <p>&nbsp;{{ num }}&nbsp;</p>
+    <button type="button" v-on:click="clickMe">CLICK</button>
+  </div>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+}
+.abcc {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .logo {
